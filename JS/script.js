d6a4648c8a25fa103c4c2fa517fd15f15221bc12
shekar88 <47589434+shekar88@users.noreply.github.com>
2020-07-09 10:25:40 +0530

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -6,6 +6,38 @@ let myBgColor = ['primary','success','secondary','warning','info','danger','dark
 var newObjArr = [];
 Ordinal(myArray);
 
+function Ordinal(n) {
+    var obj = [];
+    // Using loop to insert key
+    // value in Object
+    for(var i = 0; i < keys.length; i++){
+        obj[keys[i]] = a[i]+ ','+ myBgColor[i];
+	}
+
+    // Printing object
+    for (var key of Object.keys(obj)) {
+        newObjArr[key] = obj[key].split(',');
+	}
+
+    $('#target-area-here').html(`
+		<div class="row">
+			<div class="cell col-lg-4 col-md-4 col-sm-4 col-12 bg-${newObjArr[1][1]}" id="cell1">${newObjArr[1][0]}</div>
+			<div class="cell col-lg-4 col-md-4 col-sm-4 col-12 bg-${newObjArr[2][1]}" id="cell2">${newObjArr[2][0]}</div>
+			<div class="cell col-lg-4 col-md-4 col-sm-4 col-12 bg-${newObjArr[3][1]}" id="cell3">${newObjArr[3][0]}</div>
+		</div>
+		<div class="row">
+			<div class="cell col-lg-4 col-md-4 col-sm-4 col-12 bg-${newObjArr[4][1]}" id="cell4">${newObjArr[4][0]}</div>
+			<div class="cell col-lg-4 col-md-4 col-sm-4 col-12 bg-${newObjArr[5][1]}" id="cell5">${newObjArr[5][0]}</div>
+			<div class="cell col-lg-4 col-md-4 col-sm-4 col-12 bg-${newObjArr[6][1]}" id="cell6">${newObjArr[6][0]}</div>
+		</div>
+		<div class="row">
+			<div class="cell col-lg-4 col-md-4 col-sm-4 col-12 bg-${newObjArr[7][1]}" id="cell7">${newObjArr[7][0]}</div>
+			<div class="cell col-lg-4 col-md-4 col-sm-4 col-12 bg-${newObjArr[8][1]}" id="cell8">${newObjArr[8][0]}</div>
+			<div class="cell col-lg-4 col-md-4 col-sm-4 col-12 bg-${newObjArr[9][1]}" id="cell9">${newObjArr[9][0]}</div>
+		</div>`);
+    return newObjArr;
+}
+
 //Shuffle Button
 let shuffleButton = document.querySelector('#shuffle-btn');
 shuffleButton.addEventListener('click',function() {
@@ -56,36 +88,3 @@ function sortArray(keys) {
 	}
 	Ordinal(newObjArr);
 }
-
-function Ordinal(n) {
-    var obj = [];
-    // Using loop to insert key
-    // value in Object
-    for(var i = 0; i < keys.length; i++){
-        obj[keys[i]] = a[i]+ ','+ myBgColor[i];
-	}
-
-    // Printing object
-    for (var key of Object.keys(obj)) {
-        newObjArr[key] = obj[key].split(',');
-	}
-
-    $('#target-area-here').html(`
-		<div class="row">
-			<div class="cell col-lg-4 col-md-4 col-sm-4 col-12 bg-${newObjArr[1][1]}" id="cell1">${newObjArr[1][0]}</div>
-			<div class="cell col-lg-4 col-md-4 col-sm-4 col-12 bg-${newObjArr[2][1]}" id="cell2">${newObjArr[2][0]}</div>
-			<div class="cell col-lg-4 col-md-4 col-sm-4 col-12 bg-${newObjArr[3][1]}" id="cell3">${newObjArr[3][0]}</div>
-		</div>
-		<div class="row">
-			<div class="cell col-lg-4 col-md-4 col-sm-4 col-12 bg-${newObjArr[4][1]}" id="cell4">${newObjArr[4][0]}</div>
-			<div class="cell col-lg-4 col-md-4 col-sm-4 col-12 bg-${newObjArr[5][1]}" id="cell5">${newObjArr[5][0]}</div>
-			<div class="cell col-lg-4 col-md-4 col-sm-4 col-12 bg-${newObjArr[6][1]}" id="cell6">${newObjArr[6][0]}</div>
-		</div>
-		<div class="row">
-			<div class="cell col-lg-4 col-md-4 col-sm-4 col-12 bg-${newObjArr[7][1]}" id="cell7">${newObjArr[7][0]}</div>
-			<div class="cell col-lg-4 col-md-4 col-sm-4 col-12 bg-${newObjArr[8][1]}" id="cell8">${newObjArr[8][0]}</div>
-			<div class="cell col-lg-4 col-md-4 col-sm-4 col-12 bg-${newObjArr[9][1]}" id="cell9">${newObjArr[9][0]}</div>
-		</div>`);
-    return newObjArr;
-}
-
